ed92ac672d0bfa255aae741d4e80bc40a19fbe61
Prashant rai <74197884+pisu5@users.noreply.github.com>
2026-02-15 14:11:42 +0530

    Update BASE_URL to use HTTPS for production

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const BASE_URL =
   window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1"
     ? "http://localhost:8000" // Changed from 127.0.0.1 to localhost
-    : "cb-lake.vercel.app";
+    : "https://cb-lake.vercel.app";
 
 export const api = axios.create({
   baseURL: BASE_URL,
